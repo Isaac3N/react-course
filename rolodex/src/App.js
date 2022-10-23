d9220be6a4d14019cced996c8998289c05067666
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-10-23 14:46:41 +0100

    mapping arrays to elements

--- rolodex/src/App.js
+++ rolodex/src/App.js
@@ -6,36 +6,28 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			name: { firstName: "ThinkBoi", lastName: "Paint" },
-			company: "Vilidin Studios",
+			monsters: [
+				{
+					name: "Linda",
+				},
+				{
+					name: "Frank",
+				},
+				{
+					name: "Jacky",
+				},
+				{
+					name: "jk",
+				},
+			],
 		};
 	}
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Hi {this.state.name.firstName}, {this.state.name.lastName}, I work
-						at {this.state.company}
-					</p>
-					<button
-						onClick={() => {
-							this.setState(
-								() => {
-									return {
-										name: { firstName: "Andrei", lastName: "Neaogie" },
-									};
-								},
-								() => {
-									console.log(this.state);
-								} //secondary callback
-							);
-						}}
-					>
-						Change Name
-					</button>
-				</header>
+				{this.state.monsters.map((monster) => {
+					return <h1>{monster.name}</h1>;
+				})}
 			</div>
 		);
 	}
