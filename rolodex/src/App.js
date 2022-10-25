a80927c8af037a28045ede7bc4786f5ac0c08166
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-10-25 21:44:32 +0100

    search box

--- rolodex/src/App.js
+++ rolodex/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
 	constructor() {
@@ -37,12 +38,7 @@ class App extends Component {
 		});
 		return (
 			<div className="App">
-				<input
-					className="search-box"
-					type="search"
-					placeholder="search monsters"
-					onChange={onSearchChange}
-				/>
+				<SearchBox onChangeHandler={onSearchChange} />
 				<CardList monsters={filteredMonsters} />
 			</div>
 		);
