049467b5d9e3fd6caeda78d3702540062f89e137
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-10-25 19:03:28 +0100

    card list component

--- rolodex/src/App.js
+++ rolodex/src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
 
 class App extends Component {
 	constructor() {
@@ -9,21 +9,14 @@ class App extends Component {
 			monsters: [],
 			searchField: "",
 		};
-		console.log("constructor");
 	}
 
 	componentDidMount() {
-		console.log("componentDidMount");
 		fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
 			response.json().then((users) =>
-				this.setState(
-					() => {
-						return { monsters: users };
-					},
-					() => {
-						console.log(this.state);
-					}
-				)
+				this.setState(() => {
+					return { monsters: users };
+				})
 			)
 		);
 	}
@@ -36,7 +29,6 @@ class App extends Component {
 	};
 
 	render() {
-		console.log("render");
 		const { monsters, searchField } = this.state;
 		const { onSearchChange } = this;
 
@@ -51,13 +43,7 @@ class App extends Component {
 					placeholder="search monsters"
 					onChange={onSearchChange}
 				/>
-				{filteredMonsters.map((monster) => {
-					return (
-						<div key={monster.id}>
-							<h1>{monster.name}</h1>
-						</div>
-					);
-				})}
+				<CardList monsters={filteredMonsters} />
 			</div>
 		);
 	}
