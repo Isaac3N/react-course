77792795c0ca1b6221905bd6f0d6d6dd4260e095
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-12-15 06:48:27 +0100

    sign in with google redirect with firebase

--- ecommerce/src/routes/sign-in/SignIn.jsx
+++ ecommerce/src/routes/sign-in/SignIn.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase'
+import React, {useEffect} from 'react'
+import { getRedirectResult } from "firebase/auth"
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from '../../utils/firebase/firebase'
 
-const signIn = () => {
+const SignIn = () => {
+    useEffect(()=> {
+        async function getResult(){
+            const response = await getRedirectResult(auth);
+            console.log(response);
+            if (response){
+                const userDocRef = await createUserDocumentFromAuth(response.user)
+            }
+        }
+        getResult()
+    }, [])
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
+    }
+
+    const logGoogleRedirectUser= async () => {
+        const {user} = await signInWithGoogleRedirect();
+        console.log({user})
     }
   return (
     <div>
@@ -12,9 +29,14 @@ const signIn = () => {
         <button onClick={logGoogleUser}>
             sign in with google popup
         </button>
+
+        <button onClick={signInWithGoogleRedirect}>
+            sign in with google redirect
+        </button>
+        
     </div>
     
   )
 }
 
-export default signIn
+export default SignIn
