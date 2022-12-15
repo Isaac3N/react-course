244f0a54a279a51a7d9d402ed7f7a5e92d72d12e
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-12-15 18:46:29 +0100

    created authentication page component alongside sign in form

--- ecommerce/src/components/form-input/FormInput.jsx
+++ ecommerce/src/components/form-input/FormInput.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import "./form-input.scss"
+import React from "react";
+import "./form-input.scss";
 
-const FormInput = ({label, ...otherProps}) => {
-  return (
-    <div className='group'>
-        <input className='form-input'{...otherProps}/>
-        {label && (
-        <label className={`${otherProps.value.length? "shrink": ""} form-input-label`}>{label}</label>)}
-    </div>
-  )
-}
+const FormInput = ({ label, ...otherProps }) => {
+	return (
+		<div className="group">
+			<input className="form-input" {...otherProps} />
+			{label && (
+				<label
+					className={`${
+						otherProps.value.length ? "shrink" : ""
+					} form-input-label`}
+				>
+					{label}
+				</label>
+			)}
+		</div>
+	);
+};
 
-export default FormInput
+export default FormInput;
