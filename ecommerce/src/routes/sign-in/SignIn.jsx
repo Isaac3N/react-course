414a12f0854a40b8bb48e4893d344ebc0d50acc1
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-12-15 16:45:51 +0100

    passing props using the input options

--- ecommerce/src/routes/sign-in/SignIn.jsx
+++ ecommerce/src/routes/sign-in/SignIn.jsx
@@ -1,27 +1,12 @@
-import React, {useEffect} from 'react'
-import { getRedirectResult } from "firebase/auth"
-import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from '../../utils/firebase/firebase'
+import React from 'react'
+import SignUpForm from '../../components/sign-up-form/SignUpForm';
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase'
 
 const SignIn = () => {
-    useEffect(()=> {
-        async function getResult(){
-            const response = await getRedirectResult(auth);
-            console.log(response);
-            if (response){
-                const userDocRef = await createUserDocumentFromAuth(response.user)
-            }
-        }
-        getResult()
-    }, [])
 
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
-    }
-
-    const logGoogleRedirectUser= async () => {
-        const {user} = await signInWithGoogleRedirect();
-        console.log({user})
     }
   return (
     <div>
@@ -29,10 +14,7 @@ const SignIn = () => {
         <button onClick={logGoogleUser}>
             sign in with google popup
         </button>
-
-        <button onClick={signInWithGoogleRedirect}>
-            sign in with google redirect
-        </button>
+        <SignUpForm/>
         
     </div>
     
