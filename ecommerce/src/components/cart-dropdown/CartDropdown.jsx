9344a1aecdb4036be60ece492c20840dd0f3ac6f
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-01-06 11:09:33 +0100

    created checkout page along with checkout items and checkout cart functionality

--- ecommerce/src/components/cart-dropdown/CartDropdown.jsx
+++ ecommerce/src/components/cart-dropdown/CartDropdown.jsx
@@ -3,9 +3,15 @@ import { CartContext } from "../../contexts/CartContext";
 import "./cart-dropdown.scss";
 import Button from "../button/Button";
 import CartItem from "../cart-item/CartItem";
+import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
 	const { cartItems } = useContext(CartContext);
+	const navigate = useNavigate();
+
+	const goToCheckOutHandler = () => {
+		navigate("/checkout");
+	};
 
 	return (
 		<div className="cart-dropdown-container">
@@ -14,7 +20,7 @@ const CartDropdown = () => {
 					<CartItem key={item.id} cartItem={item} />
 				))}
 			</div>
-			<Button>CHECKOUT</Button>
+			<Button onClick={goToCheckOutHandler}>CHECKOUT</Button>
 		</div>
 	);
 };
---
+++ ecommerce/src/components/checkout/Checkout.jsx
@@ -0,0 +1,37 @@
+import "./checkout.scss";
+
+import React, { useContext } from "react";
+import { CartContext } from "../../contexts/CartContext";
+import CheckoutItem from "../checkout-item/CheckoutItem";
+
+const Checkout = () => {
+	const { cartItems, cartTotal } = useContext(CartContext);
+
+	return (
+		<div className="checkout-container">
+			<div className="checkout-header">
+				<div className="header-block">
+					<span>Product</span>
+				</div>
+				<div className="header-block">
+					<span>Description</span>
+				</div>
+				<div className="header-block">
+					<span>quantity</span>
+				</div>
+				<div className="header-block">
+					<span>price</span>
+				</div>
+				<div className="header-block">
+					<span>Remove</span>
+				</div>
+			</div>
+			{cartItems.map((cartItem) => {
+				return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
+			})}
+			<span className="total">Total: ${cartTotal}</span>
+		</div>
+	);
+};
+
+export default Checkout;
