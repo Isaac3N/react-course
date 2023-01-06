fc6b424abcb26a29237e39e09105ef78a9cfe2c5
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-01-06 09:00:21 +0100

    added in checkout feature

--- ecommerce/src/components/cart-icon/CartIcon.jsx
+++ ecommerce/src/components/cart-icon/CartIcon.jsx
@@ -5,13 +5,13 @@ import { CartContext } from "../../contexts/CartContext";
 import "./cart-icon.scss";
 
 const CartIcon = () => {
-	const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+	const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
 	const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 	return (
 		<div className="cart-icon-container" onClick={toggleIsCartOpen}>
 			<ShoppingIcon />
-			<span className="item-count">0</span>
+			<span className="item-count">{cartCount}</span>
 		</div>
 	);
 };
